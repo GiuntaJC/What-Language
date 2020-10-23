70178b13b61ed16dc219e1b85380ed3e87fb2d45
Joseph Giunta <jgnohack@gmail.com>
2020-10-23 09:37:48 -0700

    add question, answers & variables for both

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  let currentQuestion = "You shouldn't see this! If you do, it's a bug and you should report it!";
+  let answer1 = "You shouldn't see this! If you do, it's a bug and you should report it!";
+  let answer2 = "You shouldn't see this! If you do, it's a bug and you should report it!";
+  let answer3 = "You shouldn't see this! If you do, it's a bug and you should report it!";
+  let answer4 = "You shouldn't see this! If you do, it's a bug and you should report it!";
+
+  let cSharp;
+  let javaScript;
+  let python;
+
+  $("#currentQuestion").text(currentQuestion);
+  $("#answer1").text(answer1);
+  $("#answer2").text(answer2);
+  $("#answer3").text(answer3);
+  $("#answer4").text(answer4);
 });
