f3b17707b1e52c020a59aa3467df8c7c4e82d056
Joseph Giunta <jgnohack@gmail.com>
2020-10-23 12:46:26 -0700

    remove external functions file. It works, but not worth it for this

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
-import {hello} from js/functions.mjs;
-
 $(document).ready(function() {
-  hello();
   const bugAlert = "This text should not appear! If it does, it's a bug and should be reported!"
   const totalQuestions = 5;   // This var is handled by the if statements for determining when the quiz should end and loop. It exists so it is easier to add/remove questions
   
@@ -17,12 +14,22 @@ $(document).ready(function() {
   let python = 0;
 
   function getAnswer() {
-    checkedAnswer = $("input:radio[name=question]:checked").val();
+    checkedAnswer = parseInt($("input:radio[name=question]:checked").val());
     console.log(checkedAnswer); // DEBUG CODE
-
-    if() {
-
-    }
+  
+    if(checkedAnswer === 1) {
+      console.log("Answer 1");
+      cSharp++;
+      $("input:radio[name=question]:checked").prop("checked", false);
+    } else if(checkedAnswer === 2) {
+      console.log("Answer 2");
+      javaScript++;
+      $("input:radio[name=question]:checked").prop("checked", false);
+    } else if(checkedAnswer === 3) {
+      console.log("Answer 3");
+      python++;
+      $("input:radio[name=question]:checked").prop("checked", false);
+    } 
   }
 
   $("form#quiz").submit(function() {
@@ -38,36 +45,30 @@ $(document).ready(function() {
       answer2 = "This is the second answer";
       answer3 = "This is the third answer";
     } else if(question === 2) {
+      getAnswer();
       questionText = "This is the second question";
       answer1 = "This is the first answer";
       answer2 = "This is the second answer";
       answer3 = "This is the third answer";
     } else if(question === 3) {
+      getAnswer();
       questionText = "This is the third question";
       answer1 = "This is the first answer";
       answer2 = "This is the second answer";
       answer3 = "This is the third answer";
     } else if(question === 4) {
+      getAnswer();
       questionText = "This is the fourth question";
       answer1 = "This is the first answer";
       answer2 = "This is the second answer";
       answer3 = "This is the third answer";
     } else if(question === 5) {
+      getAnswer();
       questionText = "This is the fifth question";
       answer1 = "This is the first answer";
       answer2 = "This is the second answer";
       answer3 = "This is the third answer";
     }
-
-    // Get and record answer
-   /* if(question > 1) {
-      checkedAnswer = $("input:radio[name=question]:checked").val();
-      console.log(checkedAnswer); // DEBUG CODE
-      if(checkedAnswer === 1) {
-
-      }
-
-    } */
 
     // Show and set text if on a question, hide and reset if not
     if(question > 0 && question < (totalQuestions + 1)) {
@@ -76,6 +77,7 @@ $(document).ready(function() {
       $("#answer2").text(answer2);
       $("#answer3").text(answer3);
     } else if (question === (totalQuestions + 1)) {
+      getAnswer();
       $(".radio").hide();
       $("#button").val("Start Over");
       $("#endOfQuizText").show();
@@ -86,11 +88,11 @@ $(document).ready(function() {
       $("#button").val("Start Quiz");
       $("#endOfQuizText").hide();
       questionText = bugAlert;
+      checkedAnswer = 0;
       answer1 = bugAlert;
       answer2 = bugAlert;
       answer3 = bugAlert;
       question = 0;
-      checkedAnswer = 0;
       cSharp = 0;
       javaScript = 0;
       python = 0;
